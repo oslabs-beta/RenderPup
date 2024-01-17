cdb720e650925658ee2c7cd31f19c2e18c06bf70
Vicky Hoang <vicky.k.hoang@gmail.com>
2024-01-17 15:07:59 -0800

    Initialized webpage shell and nextJS test app

---
+++ renderpup/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Dashboard from './dashboard'
+
+const App = () => {
+  return (
+      <div>hello
+      <Dashboard/>
+      </div>
+  )
+}
+
+export default App;
---
+++ renderpup/src/components/dashboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const dashboard = () => {
+  return (
+    <div>dashboard</div>
+  );
+};
+
+export default dashboard;
