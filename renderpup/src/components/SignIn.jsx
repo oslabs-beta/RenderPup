54581ad05461fb97ec0caada665f33148822bb6a
Charmie <charmiedubongco@gmail.com>
2024-02-06 17:27:47 -0800

    removed vite

--- renderpup/src/components/SignIn.jsx
+++ renderpup/src/components/SignIn.jsx
@@ -1,5 +1,4 @@
-// import * as React from 'react';
-import React from "react";
+import React, { useEffect } from "react";
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
